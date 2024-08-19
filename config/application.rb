6d82cf9beb834303ad96fe2609a16bf74fa3a7e2
saeusu <171134641+saeusu@users.noreply.github.com>
2024-08-19 10:28:02 +0000

    adminpass変更

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'dotenv/load'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +19,28 @@ module Minkatsu
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    if Rails.env.development? || Rails.env.test?
+      Dotenv.load
+    end    
+  end
+end
+
+require_relative "boot"
+
+require "rails/all"
+require 'dotenv/load' # dotenvの読み込み
+
+Bundler.require(*Rails.groups)
+
+module Minkatsu
+  class Application < Rails::Application
+    config.load_defaults 6.1
+
+    # dotenvの読み込みが必要です
+    # load_dotenvの代わりにloadを使うことで成功する環境があります
+    # どちらでも問題ありません
+    if Rails.env.development? || Rails.env.test?
+      Dotenv.load
+    end
   end
 end
