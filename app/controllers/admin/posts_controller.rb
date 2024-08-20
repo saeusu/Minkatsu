848710d5c0ce5fe32e708ae271b60a8a8c66ebc6
saeusu <171134641+saeusu@users.noreply.github.com>
2024-08-20 01:57:43 +0000

    2ndデプロイ+テスト完了

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,19 @@
+class Admin::PostsController < ApplicationController
+  before_action :authenticate_admin!
+  
+  def index
+    @post = Post.new
+    @posts = Post.all
+    # @user = current_user
+  end
+  
+  def show
+    @post = Post.includes(:user).find(params[:id])
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:name, :price, :body, :image)
+  end  
+end
