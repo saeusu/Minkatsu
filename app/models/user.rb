87e54a6bfdc63680b129a0750d0ea9889c38a327
saeusu <171134641+saeusu@users.noreply.github.com>
2024-08-21 11:05:53 +0000

    機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,30 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
   
+  # フォローしている関連付け
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # フォローされている関連付け
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # フォローしているユーザーを取得
+  has_many :followings, through: :relationships, source: :followed
+  # フォロワーを所得
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+
+  # 指定したユーザーをフォローする
+  def follow(user)
+    relationships.create(followed_id: user.id)
+  end
+
+  # 指定したユーザーのフォローを解除する
+  def unfollow(user)
+    relationships.find_by(followed_id: user.id).destroy
+  end
+
+  # 指定したユーザーをフォローしているかどうかを判定
+  def following?(user)
+    followings.include?(user)
+  end
+  
   def user_status
     if is_deleted == true
       "退会"
