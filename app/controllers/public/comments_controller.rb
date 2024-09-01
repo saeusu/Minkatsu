f9c2e0d582c1af0135d10b5ab7d8700ce50da782
saeusu <171134641+saeusu@users.noreply.github.com>
2024-09-01 11:00:20 +0000

    自然言語処理

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,17 +1,10 @@
 class Public::CommentsController < ApplicationController
-  # def create
-  #   post = Post.find(params[:post_id])
-  #   @comment.score = Language.get_data(comment_params[:comment])
-  #   @comment = current_user.comments.new(comment_params)
-  #   @comment.post_id = post.id
-  #   @comment.save
-  # end
-  
+
   def create
     post = Post.find(params[:post_id])
-    @comment = current_user.comments.new(comment_params)  # 先に@commentを定義
+    @comment = current_user.comments.new(comment_params)
     @comment.post_id = post.id
-    @comment.score = Language.get_data(comment_params[:comment])  # その後にscoreを設定
+    @comment.score = Language.get_data(comment_params[:comment])
     @comment.save
   end
 
