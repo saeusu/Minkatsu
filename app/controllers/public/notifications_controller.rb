36ea29d8913e06f31cbacede5d7c60091e301e45
saeusu <171134641+saeusu@users.noreply.github.com>
2024-09-28 15:39:58 +0000

    通知機能修正

--- app/controllers/public/notifications_controller.rb
+++ app/controllers/public/notifications_controller.rb
@@ -3,8 +3,8 @@ class Public::NotificationsController < ApplicationController
     
   def index
     @notifications = current_user.notifications.order(created_at: :desc)
+    @unread_notifications_exist = @notifications.exists?(checked: false)
   end
-
 
   def mark_as_read
     notification = current_user.notifications.find(params[:id])
